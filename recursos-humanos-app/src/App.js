bceaba0e264cdd9cc7dd7bdfdbfb1d8b80945c30
Juan Ignacio Alvarez <nacho1294.ji@gmail.com>
2024-01-24 20:46:41 -0300

    Desplegar la aplicación en Firebase Hosting

--- recursos-humanos-app/src/App.js
+++ recursos-humanos-app/src/App.js
@@ -3,8 +3,26 @@ import ListadoEmpleados from "./empleados/ListadoEmpleados";
 import Navegacion from "./plantilla/Navegacion";
 import AgregarEmpleado from "./empleados/AgregarEmpleado";
 import EditarEmpleado from "./empleados/EditarEmpleado";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { useEffect } from "react";
+
 
 function App() {
+  // Configuracion de firebase
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCrxzPKfRshG6DYn3culCto8xjir9pZFCs",
+      authDomain: "sistema-de-recursos-humanos.firebaseapp.com",
+      projectId: "sistema-de-recursos-humanos",
+      storageBucket: "sistema-de-recursos-humanos.appspot.com",
+      messagingSenderId: "219872491335",
+      appId: "1:219872491335:web:d3d026b8c093bb22d9be62",
+      measurementId: "G-KZTKZZTTXG"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }, []);
+  
   return (
     <div className="container">
       <BrowserRouter>
